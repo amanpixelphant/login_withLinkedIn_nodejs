6fc42bf9ba921fd312624eb51e58ecacd86baf91
amanpixelphant <aman.pixelphant@gmail.com>
2023-01-11 12:38:11 +0530

    login with likedIn

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '78db3zfxk4vwii', // your App ID
+      'clientSecret': 'PAyRrZe5SJgh4BzH', // your App Secret
+      'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+    }
+  }
